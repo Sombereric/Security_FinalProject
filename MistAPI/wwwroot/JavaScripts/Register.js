9c4c786d17fda81b95b22f9cbd4f87519bca06c6
Sombereric <Emoutoux6938@conestogac.on.ca>
2026-04-16 00:33:04 -0400

    Add CSS and update auth/store pages
    Add Main.css, Login.css and Store.css to provide base layout, login/register styling and store/game item styles; remove the old Styles.css. Update Login.html and Register.html to include page headers, link the new CSS files and add basic login/register UI markup. Update Library.html and Store.html with file headers and minor comment fixes. Add/update JS file headers in Register.js and Store.js and clean up login.js. These changes prepare the frontend structure and styling for authentication and store pages.

--- MistAPI/wwwroot/JavaScripts/Register.js
+++ MistAPI/wwwroot/JavaScripts/Register.js
@@ -1,1 +1,6 @@
-﻿//creating an account functions
+﻿// FILE : Login.html
+// PROJECT : SENG2020 - MistApp
+// PROGRAMMER : Zemmat Hagos, Will Jessel, Eric Moutoux
+// FIRST VERSION : 2026-3-10
+// DESCRIPTION :
+// the login page of the program
--- MistAPI/wwwroot/JavaScripts/Store.js
+++ MistAPI/wwwroot/JavaScripts/Store.js
@@ -1,1 +1,6 @@
-﻿//used to buy games, sort store page. browse games.
+﻿// FILE : Login.html
+// PROJECT : SENG2020 - MistApp
+// PROGRAMMER : Zemmat Hagos, Will Jessel, Eric Moutoux
+// FIRST VERSION : 2026-3-10
+// DESCRIPTION :
+// the login page of the program
