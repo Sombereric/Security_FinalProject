47e4338844b111878ae1e41faab48c3a22e36a23
Wijjie <wjessel5444@conestogac.on.ca>
2026-04-15 23:41:40 -0400

    Add files via upload

---
+++ Config.js
@@ -0,0 +1,10 @@
+// config.js
+const CONFIG = {
+    API_BASE_URL: "https://api.ewicsBigWeiner.com",
+    DB_CONNECTION_STRING: "Cutie_ewic_uwu",
+    
+    // Security Settings
+    MAX_LOGIN_ATTEMPTS: 5,
+    COOLDOWN_MS: 30000, // 30 seconds
+    ILLEGAL_CHARS: /[;<>"-]/g 
+};
